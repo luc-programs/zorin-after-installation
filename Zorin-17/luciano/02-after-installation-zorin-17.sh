78579cc1cc0760140b247c4b385d7bde01c3d839
luc-programs <luc.github.programs@hotmail.com>
2024-11-28 13:15:47 -0300

    remoção da instalação do Visual Studio Codium do Script de instalacao do Zorin e colocando ele em outro arquivo

--- Zorin-17/luciano/02-after-installation-zorin-17.sh
+++ Zorin-17/luciano/02-after-installation-zorin-17.sh
@@ -112,36 +112,6 @@ echo "**************************************************************************
 
 
 
-echo -e "\n\n\n ***********************************************  INSTALANDO  VISUAL  STUDIO  CODIUM  ************************************************"
-
-## Entrando no diretorio com as extencoes
-cd ~/Installation/vscode-vscodium-extensions
-
-
-
-sudo dpkg -i codium_1.94.2.24286_amd64.deb
-sudo apt --fix-broken install -y
-
-## Abringdo o Codium
-codium
-
-tempoParado=10
-echo "Parando o Script por $tempoParado abrir o Codium";
-sleep $tempoParado;
-echo "Continuando execucao do Script"
-
-
-## Copiando o arquivo de configuracao do VsCode para o VsCodium
-cp ~/.config/Code/User/settings.json ~/.config/VSCodium/User/
-
-
-## Entrando na para pasta de programas
-cd ~/Downloads/Softwares/
-
-echo "***********************************************************************************************************************************************"
-
-
-
 echo -e "\n\n\n************************************************** INSTALANDO  FLUTTER*****************************************************************"
 
 if [ -e flutter_linux_3.24.3-stable.tar.xz ];
