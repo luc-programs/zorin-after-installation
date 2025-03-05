8d2abb4920c10732ecff594e879c2b47d2951a34
luc-programs <luc.github.programs@hotmail.com>
2025-03-05 09:26:41 -0300

    add installation o Pycharm

--- Zorin-17/luciano/01-after-installation-zorin-17.sh
+++ Zorin-17/luciano/01-after-installation-zorin-17.sh
@@ -829,6 +829,18 @@ echo -e "\n\n\n#################################################################
 
 
 
+echo -e "\n\n\n######################################################################## INSTALACAO  PYCHARM #######################################################################"
+
+sudo apt install pycharm-professional -y
+
+## Mudando o arquivo executavel que é um shell script para o arquivo binario
+sudo sed -ie 's\pycharm-professional .*%u\/opt/pycharm-professional/bin/pycharm\g' /usr/share/applications/pycharm-professional.desktop
+
+echo -e "\n\n\n####################################################################################################################################################################"
+
+
+
+
 echo -e "\n\n\n#################################################################### INSTALACAO  ANDROID  STUDIO ##################################################################"
 
 ## Adicionando repositorio do Android Studio
