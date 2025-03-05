57a9925902d92f149b65fd13fbd7302a245ac58b
luc-programs <luc.github.programs@hotmail.com>
2025-03-05 09:23:42 -0300

    add installation of PHP Storm

--- Zorin-17/luciano/01-after-installation-zorin-17.sh
+++ Zorin-17/luciano/01-after-installation-zorin-17.sh
@@ -816,6 +816,19 @@ echo -e "\n\n\n#################################################################
 
 
 
+
+echo -e "\n\n\n###################################################################### INSTALACAO  PHP  STORM ######################################################################"
+
+sudo apt install phpstorm -y
+
+## Mudando o arquivo executavel que é um shell script para o arquivo binario
+sudo sed -ie 's\phpstorm .*%u\/opt/phpstorm/bin/phpstorm\g' /usr/share/applications/phpstorm.desktop
+
+echo -e "\n\n\n####################################################################################################################################################################"
+
+
+
+
 echo -e "\n\n\n#################################################################### INSTALACAO  ANDROID  STUDIO ##################################################################"
 
 ## Adicionando repositorio do Android Studio
